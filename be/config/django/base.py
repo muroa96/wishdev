b359726ac3ccf36f4f0aea7b49b2c1507e0dbc67
muroa96 <thsgskwhd@gmail.com>
2024-09-24 16:34:44 +0900

    Rename: 'lms' to 'wishdev'

--- be/config/django/base.py
+++ be/config/django/base.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "lms",
+        "NAME": "wishdev",
         "USER": "root",
         "PASSWORD": "root",
         "HOST": "localhost",
